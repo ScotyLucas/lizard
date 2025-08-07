8869bc1b64c3d5e15094a1169dc2e97c3940b92f
Scoty_Lucas <vidiczkimate1@gmail.com>
2025-08-07 18:53:12 +0200

    Wiwiwi added

--- soundplay.js
+++ soundplay.js
@@ -1,6 +1,12 @@
-    const playButton = document.getElementById('playButton');
-    const myAudio = document.getElementById('myAudio');
+    const playLizard = document.getElementById('playLizard');
+    const playWiwiwi = document.getElementById('playWiwiwi');
+    const Lizard = document.getElementById('Lizard');
+    const Wiwiwi = document.getElementById('Wiwiwi');
 
-    playButton.addEventListener('click', () => {
-        myAudio.play();
+    playLizard.addEventListener('click', () => {
+        Lizard.play();
+    });
+
+    playWiwiwi.addEventListener('click', () => {
+        Wiwiwi.play();
     });
