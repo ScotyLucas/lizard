fc0314f008a9f1d9df9e81fd7798f53ac57517d0
Scoty_Lucas <vidiczkimate1@gmail.com>
2025-08-09 22:21:26 +0200

    Moreee soundssssssss

--- soundplay.js
+++ soundplay.js
@@ -1,11 +1,22 @@
+    //buttons to play
+    
     const playLizard = document.getElementById('playLizard');
     const playWiwiwi = document.getElementById('playWiwiwi');
     const playBudosc = document.getElementById('playBudosc');
     const playCsipicsipi = document.getElementById('playCsipicsipi');
+    const playNoopolizia = document.getElementById('playNoopolizia');
+    const playThungSahur = document.getElementById('playThungSahur');
+    const playHappyhappy = document.getElementById('playHappyhappy');
+
+    //sound
+
     const Lizard = document.getElementById('Lizard');
     const Wiwiwi = document.getElementById('Wiwiwi');
     const Budosc = document.getElementById('Budosc');
     const Csipicsipi = document.getElementById('Csipicsipi');
+    const Noopolizia = document.getElementById('Noopolizia');
+    const ThungSahur = document.getElementById('ThungSahur');
+    const Happyhappy = document.getElementById('Happyhappy');
 
     playLizard.addEventListener('click', () => {
         Lizard.play();
@@ -21,4 +32,16 @@
 
     playCsipicsipi.addEventListener('click', () => {
         Csipicsipi.play();
+    });
+
+    playNoopolizia.addEventListener('click', () => {
+        Noopolizia.play();
+    });
+
+    playThungSahur.addEventListener('click', () => {
+        ThungSahur.play();
+    });
+
+    playHappyhappy.addEventListener('click', () => {
+        Happyhappy.play();
     });
